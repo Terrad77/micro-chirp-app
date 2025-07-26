eb9befcae15bcd1ac2b89c32f88294f8028c9a28
DAT <terlovoy77@gmail.com>
2025-07-26 16:23:54 +0200

    fix: delete migartion files index

--- backend/src/db/index.js
+++
@@ -1,5 +0,0 @@
-import Knex from "knex";
-import knexConfig from "../../knexfile.cjs";
-// configure Knex with the development settings
-const knex = Knex(knexConfig.development);
-export default knex;
