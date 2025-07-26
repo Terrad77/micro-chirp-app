6c2dba10bc5cf47fd82ca28a6873c12af17e59be
DAT <terlovoy77@gmail.com>
2025-07-26 18:58:14 +0200

    Final checks and push for Render deploy after successful local build

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,23 +1,17 @@
-import Knex from "knex"; // Імпортуємо Knex
-import knexConfig from "../knexfile.cjs"; // Імпортуємо конфігурацію Knexfile
+import Knex from "knex";
+import knexConfig from "../knexfile.cjs";
 
-// Визначаємо середовище (development або production)
 const environment = process.env.NODE_ENV || "development";
 const config = knexConfig[environment];
 
-// перевірка, щоб переконатися, що 'config' не є 'undefined'
 if (!config) {
   console.error(
     `Knex configuration for environment "${environment}" not found in knexfile.cjs.`
   );
-  // Це зупинить виконання програми, якщо конфігурація відсутня, що є кращим, ніж використання undefined
   throw new Error(
     `Invalid Knex configuration: Environment "${environment}" is not defined.`
   );
 }
 
-// Ініціалізуємо Knex
 const knex = Knex(config);
-
-// Експортуємо ініціалізований Knex-інстанс
 export default knex;
--- backend/src/knexfile.cjs
+++ backend/src/knexfile.cjs
@@ -1,6 +1,6 @@
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "..", ".env") }); // path to  .env file
-// console.log("DATABASE_URL from .env:", process.env.DATABASE_URL); // Тимчасовий лог для дебагу
+console.log("DATABASE_URL from .env:", process.env.DATABASE_URL); // for debugging
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
@@ -12,15 +12,19 @@ const config = {
       process.env.DATABASE_URL ||
       "postgresql://user:password@localhost:5432/micro_chirp_db",
     migrations: {
-      directory: "./src/db/migrations",
+      directory: "./db/migrations",
     },
     seeds: {
-      directory: "./src/db/seeds",
+      directory: "./db/seeds",
     },
     pool: {
       min: 2,
       max: 10,
-      acquireTimeoutMillis: 30000,
+      acquireTimeoutMillis: 60000,
+      createTimeoutMillis: 30000,
+      destroyTimeoutMillis: 5000,
+      idleTimeoutMillis: 30000,
+      reapIntervalMillis: 1000,
     },
   },
   // production:
@@ -28,15 +32,19 @@ const config = {
     client: "pg",
     connection: process.env.DATABASE_URL,
     migrations: {
-      directory: "./src/db/migrations",
+      directory: "./db/migrations",
     },
     seeds: {
-      directory: "./src/db/seeds",
+      directory: "./db/seeds",
     },
     pool: {
       min: 2,
       max: 10,
-      acquireTimeoutMillis: 30000,
+      acquireTimeoutMillis: 60000,
+      createTimeoutMillis: 30000,
+      destroyTimeoutMillis: 5000,
+      idleTimeoutMillis: 30000,
+      reapIntervalMillis: 1000,
     },
   },
 };
